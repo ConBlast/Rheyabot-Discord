b61a2352872ea9c7781bd7514bca36a38dae62a8
Christian Castro <lagseeing_99@hotmail.com>
2023-06-23 01:16:25 -0500

    Respuesta a mensajes en discord.js v14

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
+const { Client, Events, GatewayIntentBits, SlashCommandBuilder , Partials } = require('discord.js');
+
+const client = new Client({ 
+  intents: [Object.keys(GatewayIntentBits)],
+  partials: [Object.keys(Partials)],
+});
+
 const { token } = require('./config.json');
-const { Client, GatewayIntentBits, SlashCommandBuilder } = require('discord.js');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.once(Events.ClientReady, c => {
+  console.log(`Bot registrado como: ${c.user.tag}`);
+})
 
-client.once('ready', () => {
-  console.log(`Bot registrado como: ${client.user.tag}`);
-
-  const roarCommand = new SlashCommandBuilder()
-    .setName('roar')
-    .setDescription('RHEEEEEEEEEAH!!!!!!');
-
-  const data = roarCommand.toJSON();
-
-  client.guilds.cache.forEach((guild) => {
-    guild.commands.create(data).then((command) => {
-      console.log(`Comnado registrado: ${command.name}`);
-    }).catch(console.error);
-  });
-});
-
-client.on('interactionCreate', (interaction) => {
-  if (!interaction.isCommand()) return;
-
-  if (interaction.commandName === 'roar') {
-    interaction.reply('RHEEEEEEEEEAH!!!!!!', { ephemeral: true });
+ client.on('messageCreate', async message =>{
+  if(message.content == 'Rheya saluda'){
+    message.reply({content:"RHAAAAAAAAAAAAAAAAAAAAAAAAAH"})
   }
-});
+ })
+
+ client.on('messageCreate', async message =>{
+  if(message.content == 'ping'){
+    message.reply({content:"pong"})
+  }
+ })
+
+ client.on('messageCreate', async message =>{
+  if(message.content == 'letsgoo'){
+    message.reply({content:"LET'S GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"})
+  }
+ })
 
 client.login(token);
-
-console.log('Hola');
