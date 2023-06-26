24f5a249de425ae9eca5be96840b30e0ad4a4211
Christian Castro <lagseeing_99@hotmail.com>
2023-06-26 10:17:53 -0500

    Botones en Discord.js v14

--- index.js
+++ index.js
@@ -1,32 +1,88 @@
-const { Client, Events, GatewayIntentBits, SlashCommandBuilder , Partials } = require('discord.js');
-
-const client = new Client({ 
-  intents: [Object.keys(GatewayIntentBits)],
-  partials: [Object.keys(Partials)],
-});
+const {
+  Client,
+  Events,
+  GatewayIntentBits,
+  SlashCommandBuilder,
+  Partials,
+  IntentsBitField,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+  ComponentType
+} = require('discord.js');
 
 const { token } = require('./config.json');
 
+const client = new Client({
+  intents: [
+    Object.keys(GatewayIntentBits),
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+  partials: [Object.keys(Partials)],
+});
+
 client.once(Events.ClientReady, c => {
   console.log(`Bot registrado como: ${c.user.tag}`);
-})
+});
 
- client.on('messageCreate', async message =>{
-  if(message.content == 'Rheya saluda'){
-    message.reply({content:"RHAAAAAAAAAAAAAAAAAAAAAAAAAH"})
+// Interacciones del bot
+client.on('messageCreate', async message => {
+  if (message.content == 'Rheya saluda') {
+    message.reply({ content: "RHAAAAAAAAAAAAAAAAAAAAAAAAAH" });
   }
- })
+});
 
- client.on('messageCreate', async message =>{
-  if(message.content == 'ping'){
-    message.reply({content:"pong"})
-  }
- })
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
 
- client.on('messageCreate', async message =>{
-  if(message.content == 'letsgoo'){
-    message.reply({content:"LET'S GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"})
-  }
- })
+  if (message.content !== 'ping') return;
+
+  const firstButton = new ButtonBuilder()
+    .setLabel('Destruír el mundo')
+    .setStyle(ButtonStyle.Danger)
+    .setCustomId('first-button');
+
+  const secondButton = new ButtonBuilder()
+    .setLabel('Salvar el mundo')
+    .setStyle(ButtonStyle.Primary)
+    .setCustomId('second-button');
+
+  const buttonRow = new ActionRowBuilder().addComponents(firstButton, secondButton);
+
+  const reply = await message.reply({ content: 'Elige un botón, amo', components: [buttonRow] });
+
+  const filter = (i) => i.user.id === message.author.id;
+
+  const collector = reply.createMessageComponentCollector({
+    componentType: ComponentType.Button,
+    filter,
+    time: 10_000,
+  });
+
+  collector.on('collect', (interaction) => {
+    if (interaction.customId === 'first-button') {
+      interaction.reply('DESTRUYAMOSLO TODO');
+      return;
+    }
+
+    if (interaction.customId === 'second-button') {
+      interaction.reply('Que aburrido...');
+      return;
+    }
+  });
+
+  collector.on('end', () => {
+    firstButton.setDisabled(true);
+    secondButton.setDisabled(true);
+
+    reply.edit({
+      content: 'Me cansé de esperar, chau',
+      components: [buttonRow],
+    });
+  });
+});
 
 client.login(token);
